d7f922623ad3771051d652ef19deb9ee9a5f4c7f
Fabricio Sandoval <sandovalfabri2023@gmail.com>
2026-04-16 22:41:11 -0600

    server correigido

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.listen(PORT, () => {
  */
 
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Servidor corriendo en puerto ${PORT}`);
